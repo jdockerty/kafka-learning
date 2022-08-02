0d93432f9442e8e2c101d4ff05ad2b00f4b7bda5
Jack Dockerty <jdockerty19@gmail.com>
2022-08-02 22:00:58 +0100

    add tweaked confluent kafka producer/consumer files

---
+++ cmd/consumer/consumer.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+var (
+	brokerAddress *string = flag.String("brokers", "localhost:9092", "address of the kafka broker")
+	topic         *string = flag.String("topic", "", "the kafka topic name to send messages to")
+)
+
+func main() {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": *brokerAddress,
+		"group.id":          "foo",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		fmt.Errorf("Unable to create consumer: %s\n", err)
+		return
+	}
+
+	err = consumer.SubscribeTopics([]string{*topic}, nil)
+
+	// Set up a channel for handling Ctrl-C, etc
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Process messages
+	run := true
+	for run == true {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev, err := consumer.ReadMessage(100 * time.Millisecond)
+			if err != nil {
+				// Errors are informational and automatically handled by the consumer
+				continue
+			}
+			fmt.Printf("Consumed event from topic %s: key = %-10s value = %s\n",
+				*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+		}
+	}
+
+	consumer.Close()
+
+}
